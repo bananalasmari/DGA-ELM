f969208b0ddd04d56f20d7420e8d79fc9faf32e6
Banan Alasmari <benoswaggy@gmail.com>
2024-07-25 13:51:49 +0300

    Project Files

---
+++ src/app/_layouts/inner-layout/inner-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inner-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './inner-layout.component.html',
+  styleUrl: './inner-layout.component.scss'
+})
+export class InnerLayoutComponent {
+
+}
