e65de6f13f29381d0dbded3d6321c4cd266ad57c
Banan Alasmari <benoswaggy@gmail.com>
2024-07-25 14:28:35 +0300

    Routes Ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './_pages/landing/landing.component';
+import { AppointmentsComponent } from './_pages/appointments/appointments.component';
+import { InnerLayoutComponent } from './_layouts/inner-layout/inner-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: InnerLayoutComponent,
+        children: [
+            { path: 'appointment', component: AppointmentsComponent},
+    
+        ]
+      },
+];
