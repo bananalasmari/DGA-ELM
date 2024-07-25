bae7374e5fcd35aa9491aab0a3a5e14f43b76330
Banan Alasmari <benoswaggy@gmail.com>
2024-07-25 19:34:36 +0300

    Dynamic App Page

--- src/app/_pages/appointments/appointments.component.ts
+++ src/app/_pages/appointments/appointments.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-appointments',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './appointments.component.html',
   styleUrl: './appointments.component.scss'
 })
-export class AppointmentsComponent {
+export class AppointmentsComponent implements OnInit {
+  title: string = 'مواعيد الأحوال';
+  welcomeMessage: string = 'مرحبا بك بخدمة المواعيد للأحوال المدنية';
+  description: string = 'خدمة المواعيد تسمح للمواطن حجز موعد بفروع الأحوال المدنية لإجراء أي خدمة والإستفادة من المميزات التالية:';
+  features: string[] = [
+    'حجز موعد جديد',
+    'التحديث على موعد حالي',
+    'الإطلاع / طباعة موعد حالي',
+    'إلغاء موعد حالي'
+  ];
+  videoLink: string = '#';
+  videoLabel: string = 'عرض الفيديو التوضيحي للخدمة';
+  buttonLabel: string = 'الإنتقال لخدمة المواعيد';
+  iconSrc: string = '../../../assets/images/app-icon.svg';
 
+  constructor() { }
+
+  ngOnInit(): void { }
 }
