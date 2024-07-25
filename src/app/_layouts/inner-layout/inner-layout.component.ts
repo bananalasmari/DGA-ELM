396843715c25d36bd25e659c73a65fb566d18590
Banan Alasmari <benoswaggy@gmail.com>
2024-07-25 19:24:00 +0300

    App Page, Header, Footer, Page Layout

--- src/app/_layouts/inner-layout/inner-layout.component.ts
+++ src/app/_layouts/inner-layout/inner-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-inner-layout',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,FooterComponent, RouterOutlet ],
   templateUrl: './inner-layout.component.html',
   styleUrl: './inner-layout.component.scss'
 })
